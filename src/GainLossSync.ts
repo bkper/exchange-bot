152c8d42a6a13474ca7fedd628259f87f52d630e
mael <mael@bkper.com>
2020-05-15 11:59:56 -0300

    Preventing npe when there is no group

--- src/GainLossSync.ts
+++ src/GainLossSync.ts
@@ -11,33 +11,37 @@ function doGet(e: GoogleAppsScript.Events.AppsScriptHttpRequestEvent) {
   connectedBooks.forEach(connectedBook => {
     let connectedCode = Service_.getBaseCode(connectedBook);
     let group = book.getGroup(connectedCode);
-    let accounts = group.getAccounts();
-    if (accounts != null) {
-      accounts.forEach(account => {
-        let connectedAccount = connectedBook.getAccount(account.getName());
-        if (connectedAccount != null) {
-          let expectedBalance = ExchangeApp.exchange(connectedAccount.getBalance()).from(connectedCode).to(baseCode).convert();
-          let delta = account.getBalance() - expectedBalance;
 
-          let excAccountName = `${connectedCode}_exchange`;
-          
-          //Verify Exchange account created
-          let accPrefix = book.getProperty('exc_acc_prefix');
-          if (accPrefix != null && accPrefix.trim() != '') {
-            excAccountName = `${accPrefix} - ${account.getName()}`;
+    if (group != null) {
+      let accounts = group.getAccounts();
+      if (accounts != null) {
+        accounts.forEach(account => {
+          let connectedAccount = connectedBook.getAccount(account.getName());
+          if (connectedAccount != null) {
+            let expectedBalance = ExchangeApp.exchange(connectedAccount.getBalance()).from(connectedCode).to(baseCode).convert();
+            let delta = account.getBalance() - expectedBalance;
+  
+            let excAccountName = `${connectedCode}_exchange`;
+            
+            //Verify Exchange account created
+            let accPrefix = book.getProperty('exc_acc_prefix');
+            if (accPrefix != null && accPrefix.trim() != '') {
+              excAccountName = `${accPrefix} - ${account.getName()}`;
+            }
+            let excAccount = book.getAccount(excAccountName)
+            if (excAccount == null) {
+              excAccount = book.createAccount(excAccountName);
+            }
+            if (Math.round(delta) > 0) {
+              book.record(`${account.getName()} ${excAccountName} ${book.formatValue(Math.abs(delta))} #exchange_loss`)
+            } else if (Math.round(delta) < 0) {
+              book.record(`${excAccountName} ${account.getName()} ${book.formatValue(Math.abs(delta))} #exchange_gain`)
+            }
           }
-          let excAccount = book.getAccount(excAccountName)
-          if (excAccount == null) {
-            excAccount = book.createAccount(excAccountName);
-          }
-          if (Math.round(delta) > 0) {
-            book.record(`${account.getName()} ${excAccountName} ${book.formatValue(Math.abs(delta))} #exchange_loss`)
-          } else if (Math.round(delta) < 0) {
-            book.record(`${excAccountName} ${account.getName()} ${book.formatValue(Math.abs(delta))} #exchange_gain`)
-          }
-        }
-      })
+        })
+      }      
     }
+
   })
 
   return HtmlService.createTemplateFromFile('GainLossSyncDone').evaluate().setTitle('Exchange Bot');;
