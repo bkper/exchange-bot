05968247932f3a4c267ce9ce079249f4a73f978e
mael <mael@bkper.com>
2020-08-20 10:38:28 -0300

    Archiving account if has transaction posted

--- src/EventHandlerAccountDeleted.ts
+++ src/EventHandlerAccountDeleted.ts
@@ -4,7 +4,11 @@ class EventHandlerAccountDeleted extends EventHandlerAccount {
     return `${bookAnchor}: ACCOUNT ${account.name} NOT Found`;
   }
   protected connectedAccountFound(baseBook: Bkper.Book, connectedBook: Bkper.Book, account: bkper.Account, connectedAccount: Bkper.Account): string {
-    connectedAccount.remove();
+    if (connectedAccount.hasTransactionPosted()) {
+      connectedAccount.setArchived(true).update();
+    } else {
+      connectedAccount.remove();
+    }
     let bookAnchor = super.buildBookAnchor(connectedBook);
     return `${bookAnchor}: ACCOUNT ${connectedAccount.getName()} DELETED`;
   }
