7f3557da08d1aa906073123f4e6fa3b37081872c
mael <mael@bkper.com>
2020-09-18 16:43:09 -0300

    Handling Group.hidden

--- src/EventHandlerGroupCreatedOrUpdated.ts
+++ src/EventHandlerGroupCreatedOrUpdated.ts
@@ -2,6 +2,7 @@ class EventHandlerGroupCreatedOrUpdated extends EventHandlerGroup {
   protected connectedGroupNotFound(baseBook: Bkper.Book, connectedBook: Bkper.Book, baseGroup: bkper.Group): string {
     let connectedGroup = connectedBook.newGroup()
     .setName(baseGroup.name)
+    .setHidden(baseGroup.hidden)
     .setProperties(baseGroup.properties)
     .create();
     let bookAnchor = super.buildBookAnchor(connectedBook);
@@ -10,6 +11,7 @@ class EventHandlerGroupCreatedOrUpdated extends EventHandlerGroup {
   protected connectedGroupFound(baseBook: Bkper.Book, connectedBook: Bkper.Book, baseGroup: bkper.Group, connectedGroup: Bkper.Group): string {
     connectedGroup
     .setName(baseGroup.name)
+    .setHidden(baseGroup.hidden)
     .setProperties(baseGroup.properties)
     .update();
     let bookAnchor = super.buildBookAnchor(connectedBook);
