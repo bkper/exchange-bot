599f2d2b90b1b91a07f0bb19522297540649c9d9
mael <mael@bkper.com>
2021-05-03 16:06:08 -0300

    Propagating parent group

--- gcf/src/EventHandlerGroupCreatedOrUpdated.ts
+++ gcf/src/EventHandlerGroupCreatedOrUpdated.ts
@@ -4,8 +4,10 @@ import { EventHandlerGroup } from "./EventHandlerGroup";
 
 export class EventHandlerGroupCreatedOrUpdated extends EventHandlerGroup {
   protected async connectedGroupNotFound(baseBook: Book, connectedBook: Book, baseGroup: bkper.Group): Promise<string> {
+    let parentGroup = baseGroup.parent ? await connectedBook.getGroup(baseGroup.parent.name) : null;
     let connectedGroup = await connectedBook.newGroup()
     .setName(baseGroup.name)
+    .setParent(parentGroup)
     .setHidden(baseGroup.hidden)
     .setProperties(baseGroup.properties)
     .deleteProperty(CHILD_BOOK_ID_PROP)
@@ -15,8 +17,11 @@ export class EventHandlerGroupCreatedOrUpdated extends EventHandlerGroup {
   }
   protected async connectedGroupFound(baseBook: Book, connectedBook: Book, baseGroup: bkper.Group, connectedGroup: Group): Promise<string> {
     let connectedChildBookId = connectedGroup.getProperty(CHILD_BOOK_ID_PROP)
+    let parentGroup = baseGroup.parent ? await connectedBook.getGroup(baseGroup.parent.name) : null;
+
     await connectedGroup
     .setName(baseGroup.name)
+    .setParent(parentGroup)
     .setHidden(baseGroup.hidden)
     .setProperties(baseGroup.properties)
     .setProperty(CHILD_BOOK_ID_PROP, connectedChildBookId)
