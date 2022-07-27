fe5359f029c6900d4afd3196be33876300852d20
victordrumond <victordrumond@hotmail.com>
2022-07-27 11:39:28 -0300

    Record exc_log prop when updating transaction

--- gcf/src/EventHandlerTransactionUpdated.ts
+++ gcf/src/EventHandlerTransactionUpdated.ts
@@ -1,6 +1,6 @@
 import { Account, Book, Transaction } from "bkper";
 import { getBaseCode } from "./BotService";
-import { EXC_CODE_PROP, EXC_RATE_PROP } from "./constants";
+import { EXC_CODE_PROP, EXC_RATE_PROP, EXC_LOG_PROP } from "./constants";
 import { AmountDescription, EventHandlerTransaction } from "./EventHandlerTransaction";
 
 export class EventHandlerTransactionUpdated extends EventHandlerTransaction {
@@ -12,6 +12,7 @@ export class EventHandlerTransactionUpdated extends EventHandlerTransaction {
   protected connectedTransactionNotFound(baseBook: Book, connectedBook: Book, baseTransaction: bkper.Transaction): Promise<string> {
     return null;
   }
+
   protected async connectedTransactionFound(baseBook: Book, connectedBook: Book, baseTransaction: bkper.Transaction, connectedTransaction: Transaction): Promise<string> {
 
     const timeTag = `Updated found ${Math.random()}`
@@ -39,12 +40,9 @@ export class EventHandlerTransactionUpdated extends EventHandlerTransaction {
       }
     }
 
-
     let amountDescription = await super.extractAmountDescription_(baseBook, connectedBook, baseCode, connectedCode, baseTransaction);
 
-    
     let amountFormatted = connectedBook.formatValue(connectedTransaction.getAmount())
-
 
     if (amountDescription.amount.eq(0)) {
       if (connectedTransaction.isChecked()) {
@@ -58,7 +56,7 @@ export class EventHandlerTransactionUpdated extends EventHandlerTransaction {
 
     let bookAnchor = super.buildBookAnchor(connectedBook);
 
-    await this.updateConnectedTransaction(connectedBook, connectedTransaction, amountDescription, baseTransaction, connectedCreditAccount, connectedDebitAccount);
+    await this.updateConnectedTransaction(baseBook, connectedBook, connectedTransaction, amountDescription, baseTransaction, connectedCreditAccount, connectedDebitAccount);
 
     let record = `EDITED: ${connectedTransaction.getDateFormatted()} ${connectedBook.formatValue(connectedTransaction.getAmount())}  ${await connectedTransaction.getCreditAccountName()} ${await connectedTransaction.getDebitAccountName()} ${connectedTransaction.getDescription()}`;
 
@@ -69,18 +67,19 @@ export class EventHandlerTransactionUpdated extends EventHandlerTransaction {
 
 
 
-private async updateConnectedTransaction(connectedBook: Book, connectedTransaction: Transaction, amountDescription: AmountDescription, transaction: bkper.Transaction, connectedCreditAccount: Account, connectedDebitAccount: Account) {
+  private async updateConnectedTransaction(baseBook: Book, connectedBook: Book, connectedTransaction: Transaction, amountDescription: AmountDescription, transaction: bkper.Transaction, connectedCreditAccount: Account, connectedDebitAccount: Account) {
 
-  if (connectedTransaction.isChecked()) {
-    await connectedTransaction.uncheck();
-  }
+    if (connectedTransaction.isChecked()) {
+      await connectedTransaction.uncheck();
+    }
 
-  connectedTransaction.setAmount(amountDescription.amount)
-    .setDescription(amountDescription.description)
-    .setDate(transaction.date)
-    .setProperties(transaction.properties)
-    .setCreditAccount(connectedCreditAccount)
-    .setDebitAccount(connectedDebitAccount);
+    connectedTransaction.setAmount(amountDescription.amount)
+      .setDescription(amountDescription.description)
+      .setDate(transaction.date)
+      .setProperties(transaction.properties)
+      .setCreditAccount(connectedCreditAccount)
+      .setDebitAccount(connectedDebitAccount)
+    ;
 
     if (amountDescription.excBaseCode) {
       connectedTransaction.setProperty(EXC_CODE_PROP, amountDescription.excBaseCode);
@@ -90,28 +89,29 @@ private async updateConnectedTransaction(connectedBook: Book, connectedTransacti
       connectedTransaction.setProperty(EXC_RATE_PROP, amountDescription.excBaseRate.toString())
     }
 
-  let urls = transaction.urls;
-  if (!urls) {
-    urls = [];
+    if (amountDescription.rates) {
+      const excLogEntries = await this.buildExcLog(baseBook, connectedBook, transaction, amountDescription);
+      if (excLogEntries.length > 0) {
+        connectedTransaction.setProperty(EXC_LOG_PROP, JSON.stringify(excLogEntries));
+      }
+    }
+
+    let urls = transaction.urls;
+    if (!urls) {
+      urls = [];
+    }
+    if (transaction.urls) {
+      urls = transaction.urls;
+    }
+    if (transaction.files) {
+      transaction.files.forEach(file => {
+        urls.push(file.url);
+      });
+    }
+
+    connectedTransaction.setUrls(urls);
+
+    await connectedTransaction.update();
   }
 
-  if (transaction.urls) {
-    urls = transaction.urls;
-  }
-
-  if (transaction.files) {
-    transaction.files.forEach(file => {
-      urls.push(file.url);
-    });
-  }
-
-  connectedTransaction
-    .setUrls(urls);
-
-  await connectedTransaction.update();
 }
-
-
-
-}
-
